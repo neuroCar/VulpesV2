9dec9270799d5227ab45c8edcf2983a556dfb461
neuroCar <n2194050189@gmail.com>
2026-01-29 22:42:35 -0600

    Fix mistake in naming

--- commands/reactions/pat.js
+++ commands/reactions/pat.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Pat')
+        .setName('pat')
         .setDescription(`Pat reaction`),
     
     async execute(interaction) {
