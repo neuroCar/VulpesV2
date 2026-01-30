8294aca6cf2c87ff9bc670a021af9fce660e2ae7
neuroCar <n2194050189@gmail.com>
2026-01-29 23:23:00 -0600

    Added DM permissions

--- deploy-commands.js
+++ deploy-commands.js
@@ -18,7 +18,9 @@ for (const folder of commandFolders) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
+			const json = command.data.toJSON();
+			json.dm_permission = true;
+			commands.push(json);
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
