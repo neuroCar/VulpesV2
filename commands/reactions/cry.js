967c86ee4dbeff6dbee84da11d87cd13aaf36b01
neuroCar <n2194050189@gmail.com>
2026-01-29 22:41:15 -0600

    Fix a mistake in naming

--- commands/reactions/cry.js
+++ commands/reactions/cry.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Cry')
+        .setName('cry')
         .setDescription(`Cry reaction`),
     
     async execute(interaction) {
