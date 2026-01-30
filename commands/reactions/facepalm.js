d8e309c098c0dae000d758df5abb866abcf470ff
neuroCar <n2194050189@gmail.com>
2026-01-30 00:02:14 -0600

    Revert "Fix mistake in api link"
    This reverts commit e2dc1ed4a28becd28d9b4a51a46d4030e06c9cdc.
    
    Returning to stable release

--- commands/reactions/facepalm.js
+++ commands/reactions/facepalm.js
@@ -7,7 +7,7 @@ module.exports = {
         .setDMPermission(true),
     
     async execute(interaction) {
-        const response = await fetch(`https://api.some-random-api.com/animu/face-palm`)
+        const response = await fetch(`https://api.some-random-api.com/animu/facepalm`)
         const data = await response.json()
         const embed = new EmbedBuilder()
             .setImage(data.link)
