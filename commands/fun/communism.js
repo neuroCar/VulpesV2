4982268204a7388767694951ccaa4be10b6d21c9
neuroCar <n2194050189@gmail.com>
2026-02-02 17:24:15 -0600

    Added 2 new commands

---
+++ commands/fun/communism.js
@@ -0,0 +1,22 @@
+const pop = require(`popcat-wrapper`);
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('communism').setDescription('This user is a communist!').addUserOption(option => option
+        .setName('user').setDescription('The communist').setRequired(false)
+    ),
+    async execute(interaction) {
+        let user = interaction.options.getUser("user") || interaction.user
+        const pfp = user.displayAvatarURL({
+            extension: `png`,
+            size: 512,
+            forceStatic: false,
+        });
+        const communist = await pop.communism(pfp);
+        
+        const embed = new EmbedBuilder()
+            .setImage(communist)
+            .setFooter({ text: "https:://popcat.xyz/api", icon: "https://cdn.popcat.xyz/popcat.png" });
+        await interaction.reply({ embeds: [embed] });
+    },
+}
--- commands/fun/petpet.js
+++ commands/fun/petpet.js
@@ -1,17 +1,37 @@
 const pop = require(`popcat-wrapper`);
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, Attachment, AttachmentBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('petpet').setDescription('Pet.'),
+    data: new SlashCommandBuilder().setName('petpet').setDescription('Pet.').addUserOption(option => option
+        .setName('user').setDescription("The user to pet").setRequired(false)
+    ),
     async execute(interaction) {
-        let user = interaction.mentions.users.first() || interaction.author;
-        const pfp = user.displayAvatarURL();
+        let user = interaction.options.getUser('user') || interaction.user;
+        const pfp = user.displayAvatarURL({
+            extension: `png`,
+            size: 512,
+            forceStatic: false,
+        });
 
-        const img = fetch(`https://api.popcat.xyz/v2/pet?image=${pfp}`);
+        const img = `https://api.popcat.xyz/v2/pet?image=${encodeURIComponent(pfp)}&type=gif`
         
+        const res = await fetch(img);
+        const arrayBuffer = await res.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        const attachment = new AttachmentBuilder(buffer, { name: `pet.gif` });
+
+        if (user === interaction.user) {
+            msg = `${user} pet themself!`
+        } else {
+            msg = `${interaction.user} pet ${user}!`
+        }
+
         const embed = new EmbedBuilder()
-            .setImage(img)
-            .setFooter({ text: "Joke from https:://popcat.xyz/api", icon: "https://cdn.popcat.xyz/popcat.png" })
-        await interaction.reply({ embeds: [embed] });
+            .addFields(
+                { name: `pet-pet`, value: msg },
+            )
+            .setImage(`attachment://pet.gif`)
+            .setFooter({ text: "Image from https:://popcat.xyz/api", icon: "https://cdn.popcat.xyz/popcat.png" })
+        await interaction.reply({ embeds: [embed], files: [attachment] });
     },
 }
